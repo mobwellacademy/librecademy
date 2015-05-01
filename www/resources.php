373d4d49add606bd7865197665c9484a077f7a2f
root <root@mail.mobwell.org>
2015-05-01 14:51:09 +0000

    latest

--- www/resources.php
+++ www/resources.php
@@ -4,7 +4,7 @@
 	include_once 'include/header.inc';
 ?>
 <div class="container">
-	<h1>Resources</h1>
+	<h1>ownCloud Drive- Save that precious byte</h1>
 <!--	<iframe src="components/Filemanager/" width="100%" height="600" seamless></iframe>	 -->
 	<iframe src="http://academy.mobwell.org/owncloud" width="100%" height="768" seamless></iframe>	
 </div>
